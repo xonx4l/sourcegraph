b080e77e10d9b7aca474d95c511013cd7c7ab661
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2023-05-26 18:20:23 +0100

    the thing

--- enterprise/dev/app/app-deps.nix
+++ enterprise/dev/app/app-deps.nix
@@ -1,55 +1,58 @@
 let
-  rust_overlay = (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"));
+  # rust_overlay = (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"));
 
-  rust_custom = (self: super:
-  {
-    rust-stable = super.rust-bin.stable.latest.default.overrideAttrs (old: {
-      propagatedBuildInputs = [];
-      depsHostHostPropagated = [];
-      depsTargetTargetPropagated = [];
-    });
-  });
+  # rust_custom = (self: super:
+  #   {
+  #     rust-stable = super.rust-bin.stable.latest.default.overrideAttrs (old: {
+  #       propagatedBuildInputs = [ ];
+  #       depsHostHostPropagated = [ ];
+  #       depsTargetTargetPropagated = [ ];
+  #     });
+  #   });
 
-  pkgs = (import <nixpkgs> {
-    overlays = [ rust_overlay rust_custom ];
+  pkgs = (import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/db38340b4d9a987db4cd4e46a537851d27bc6f44.tar.gz") {
+    # overlays = [ rust_overlay rust_custom ];
     config = {
       allowUnfree = true;
     };
     system = "aarch64-darwin";
   });
 
-  apple_libs = with pkgs.pkgsx86_64Darwin.darwin; [
-    apple_sdk.frameworks.AppKit
-    apple_sdk.frameworks.Foundation
-    apple_sdk.frameworks.CoreFoundation
-    apple_sdk.frameworks.Carbon
-    apple_sdk.frameworks.WebKit
+  apple_libs = with pkgs.pkgsx86_64Darwin.darwin.apple_sdk_11_0; [
+    frameworks.AppKit
+    frameworks.Foundation
+    frameworks.CoreFoundation
+    frameworks.Carbon
+    frameworks.WebKit
   ];
 
-  mkClangShell = pkgs.pkgsx86_64Darwin.mkShell.override { stdenv = pkgs.pkgsx86_64Darwin.clang13Stdenv; };
+  mkClangShell = pkgs.pkgsx86_64Darwin.mkShell.override { stdenv = pkgs.pkgsx86_64Darwin.darwin.apple_sdk_11_0.clang13Stdenv; };
 
-in with pkgs.pkgsx86_64Darwin; mkClangShell {
+in
+with pkgs.pkgsx86_64Darwin; mkClangShell {
   nativeBuildInputs = [
     pkg-config
-    rust-stable
+    rustc
+    cargo
+    git
+    openssh
+    which
 
-    pkgs.nodejs-16_x
-    (pkgs.nodejs-16_x.pkgs.pnpm.override {
+    nodejs-18_x
+    (nodejs-18_x.pkgs.pnpm.override {
       version = "8.1.0";
       src = fetchurl {
         url = "https://registry.npmjs.org/pnpm/-/pnpm-8.1.0.tgz";
         sha512 = "sha512-e2H73wTRxmc5fWF/6QJqbuwU6O3NRVZC1G1WFXG8EqfN/+ZBu8XVHJZwPH6Xh0DxbEoZgw8/wy2utgCDwPu4Sg==";
       };
     })
-    pkgs.nodePackages.typescript
+    nodejs-18_x.pkgs.typescript
 
     go_1_20
-  ];
+  ] ++ apple_libs;
 
-  buildInputs =  [
+  buildInputs = [
     libiconv
   ];
-  #CC="${clang13Stdenv.cc.cc}/bin/clang";
-  CARGO_TARGET_X86_64_APPLE_DARWIN_LINKER = "${clang13Stdenv.cc.cc}/bin/clang";
 }
 
