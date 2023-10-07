0eaf58815ecfbd108cdfa696f93feaa6ff0429d7
Quinn Slack <quinn@slack.org>
2023-10-08 04:06:12 +0900

    use esbuild for client/web builds (and remove webpack)

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,12 @@
 const gulp = require('gulp')
 
 const { graphQlOperations, schema, watchGraphQlOperations } = require('./client/shared/gulpfile')
-const { webpack: webWebpack, developmentServer, generate, watchGenerators } = require('./client/web/gulpfile')
+const { build, developmentServer, generate, watchGenerators } = require('./client/web/gulpfile')
 
 /**
  * Generates files needed for builds whenever files change.
  */
 const watchGenerate = gulp.series(generate, watchGenerators)
-
-/**
- * Builds everything.
- */
-const build = gulp.series(generate, webWebpack)
 
 /**
  * Watches everything and rebuilds on file changes.
